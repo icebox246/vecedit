239d23b52e0e05c04739381c0136b321810f88d2
icebox246 <iceboxpoczta@gmail.com>
2024-12-05 19:07:57 +0100

    Silence remaining warnings from raygui

--- thirdparty/raygui.cpp
+++ thirdparty/raygui.cpp
@@ -1,5 +1,6 @@
 #pragma clang diagnostic ignored "-Wdeprecated-enum-compare-conditional"
 #pragma GCC diagnostic ignored "-Wenum-compare"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
